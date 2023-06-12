8e5b2bdf1cacd6a6f1b049ee57d1216f35dd618e
Samantha Fenton <samantha.fenton@live.co.uk>
2023-06-12 16:59:36 +0100

    update

--- JavaScript.js
+++ JavaScript.js
@@ -1,16 +1,34 @@
-alert(Hi, welcome to my website!)
+
+let user = prompt("Hello you... What is your name?");
+console.log(user);
+
+const welcome = alert("Well hello there " + user + " welcome to my website, I hope you like it!");
+
+let myName = "Samantha";
+console.log(myName);
 
 
+let q1 = prompt("What is your favourite place to holiday?");
+console.log(q1);
+
+const a1 = alert("oooooh! LOVELY!! I hear " + q1 + " is nice this time of year");
 
 
+let q2 = prompt("you have to answer the following in order to proceed....  are you ready?");
+console.log(q2)
 
-function user("What is your name"){
-    let user = prompt;
-    let alert:("Hi, welcome to my website!"+ user)
+function q3 ()
+if (q2 = yes){
+    greeting = "OK...  who would win in a fight... my mum or your mum?";
+} else {
+    greeting = "Well aren't you funny!";
 }
-console.log: user;
 
-user= prompt
+
+
+
+
+
 
 
 
