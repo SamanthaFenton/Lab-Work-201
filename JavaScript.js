326ce455d57f4094d78a5b54b2e8a317aecceae5
Samantha Fenton <samantha.fenton@live.co.uk>
2023-06-12 15:33:34 +0100

    update

---
+++ JavaScript.js
@@ -0,0 +1,20 @@
+alert(Hi, welcome to my website!)
+
+
+
+
+
+function user("What is your name"){
+    let user = prompt;
+    let alert:("Hi, welcome to my website!"+ user)
+}
+console.log: user;
+
+user= prompt
+
+
+
+
+
+
+
